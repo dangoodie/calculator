61ef4e2464c289aebad2830cb7902f8cdd5ed941
dangoodie <dan.gooden.dev@gmail.com>
2023-04-17 23:31:03 +1000

    updating calculator.js to include an operator function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,10 @@
+function operate(operator, num1, num2) {
+  if (operator === "+") return add(num1, num2);
+  if (operator === "-") return subtract(num1, num2);
+  if (operator === "*") return multiply(num1, num2);
+  if (operator === "/") return divide(num1, num2);
+}
+
 function add(a, b) {
   return a + b;
 }
